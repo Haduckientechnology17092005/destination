d755725e49d065d5b60206be8c86fe2aaf39cc86
Ha Duc Kien <haduckien1709@gmail.com>
2024-09-16 01:18:47 +0700

    success

---
+++ asserts/js/main.js
@@ -0,0 +1,45 @@
+const location1 = document.getElementById('location-1');
+const location2 = document.getElementById('location-2');
+const location3 = document.getElementById('location-3');
+const location4 = document.getElementById('location-4');
+const location5 = document.getElementById('location-5');
+const location6 = document.getElementById('location-6');
+const location7 = document.getElementById('location-7');
+const location8 = document.getElementById('location-8');
+const location9 = document.getElementById('location-9');
+const location10 = document.getElementById('location-10');
+const location11 = document.getElementById('location-11');
+
+location1.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-1/';
+});
+location2.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-2/';
+});
+location3.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-3/';
+});
+location4.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-4/';
+});
+location5.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-5/';
+});
+location6.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-6/';
+});
+location7.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-7/';
+});
+location8.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-8/';
+});
+location9.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-9/';
+});
+location10.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/location-10/';
+});
+location11.addEventListener('click', () => {
+    window.location.href = 'https://haduckientechnology17092005.github.io/Transportation/';
+});
